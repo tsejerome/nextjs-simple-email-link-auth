f4e253e5a82bc8e989505d1b8ed4324a888ae51c
tsejerome <tsejerome@freehunter.hk>
2021-08-01 22:09:41 +0800

    WIP: Making client for inputting email

---
+++ components/Colours.tsx
@@ -0,0 +1,16 @@
+const Colours = {
+  Black: '#000',
+  Blue: '#292D8D',
+  OffWhite: '#F0F0F0',
+  NewWhite: '#F9F9F9',
+  White: '#fff',
+  Peach: '#F6A98F',
+  AccentCoral: '#EE6061',
+  AlertRed: '#FF0E0E',
+  LightGrey: '#A9A9A9',
+  MediumGrey: '#555555',
+  DarkGrey: '#333333',
+  BrightYellow: '#FCBF49',
+}
+
+export default Colours;

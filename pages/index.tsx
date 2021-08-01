635a7beb025e865f37081e5e8415d2a4bb6acff3
tsejerome <tsejerome@freehunter.hk>
2021-08-01 17:37:06 +0800

    Change Homepage and api to typescript

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import React from 'react'
+import styles from '../styles/Home.module.css'
+import Home from './home'
+
+export default function Homes() {
+  return (
+    <Home />
+  )
+}
